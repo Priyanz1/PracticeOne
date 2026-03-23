4d1a4389bc67cd858e42d977a3756c6821ccea7c
Priyanz1 <160339527+Priyanz1@users.noreply.github.com>
2026-03-23 23:53:05 +0530

    Enhance Latest component with sorting and filtering functionality. Added category checkboxes and sort options for product display. Updated product prices in ProductContext for better representation.

--- frontend/src/Components/Latest.jsx
+++ frontend/src/Components/Latest.jsx
@@ -1,26 +1,93 @@
 import React, { useContext, useEffect, useState } from 'react'
 import Text from '../Components/Text'
 import ProductCom from './ProductCom'
-import {ProductDataContext} from './ProductContext'
+import { ProductDataContext } from './ProductContext'
 
 function Latest() {
-    const {Product,setProduct} = useContext(ProductDataContext);
-   const [latestCollection,setlatestCollection] = useState([]);
+  const { Product, setProduct } = useContext(ProductDataContext);
+  const [latestCollection, setlatestCollection] = useState([]);
+  const [Category, setCategory] = useState([]);
+  const [sortType,setSortType] = useState('relevent');
 
-    useEffect(()=>{
-        setlatestCollection(Product.slice(0,10));
-    },[])
-  return (
-    <div id="collection" className='pt-10'>
-        <Text Head1="LATEST" Head2="COLLECTION"/>
+ const sortProduct = () => {
+  let fpCopy = Product.slice(0,10);
+  switch (sortType){
+  case 'low-high':
+    setlatestCollection(fpCopy.sort((a,b)=>(a.price - b.price)));
+    break;
+  case 'high-low':
+    setlatestCollection(fpCopy.sort((a,b)=>(b.price - a.price)));
+    break;
+  default:
+    applyfun();
+    break;
+  }
+ }
 
-        <div className=' w-[90%] lg:w-[80%] gap-2 mx-auto grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
-          {latestCollection.map((item,index)=>{return (<ProductCom key={index} id={item.id} src={item.src} dis={item.dis} price={item.price}/>)})}
+ useEffect(()=>{
+  sortProduct();
+ },[sortType])
+ 
+  const toggle = (e) => {
+    const value=e.target.value;
+    if (Category.includes(value)) {
+      setCategory((prev) => prev.filter((item) => item !== value));
+    } else {
+      setCategory(prev=> [...prev, value]);
+    }
+  };
+
+  useEffect(() => {
+    setlatestCollection(Product.slice(0, 10));
+  }, []);
+
+  const applyfun = () => {
+    let Copy = Product.slice(0, 10);
+    if (Category.length > 0) {
+      Copy = Copy.filter((item) => Category.includes(item.cat));
+    }
+    setlatestCollection(Copy);
+  };
+  useEffect(() => {
+    applyfun();
+  }, [Category, Product])
+
+return (
+  <div id="collection" className='pt-10'>
+    <Text Head1="LATEST" Head2="COLLECTION" />
+
+    <div className='flex flex-col sm:flex-row'>
+      <div>
+        <div className='ml-5 sm:px-5 pb-7 '>
+        Categories
+        <p className='flex gap-2'>
+          <input type="checkbox" value="men" onChange={toggle} />
+          men
+        </p>
+        <p className='flex gap-2'>
+          <input type="checkbox" value="women" onChange={toggle} />
+          women
+        </p>
+        <p className='flex gap-2'>
+          <input type="checkbox" value="kids" onChange={toggle} />
+          kids
+        </p>
         </div>
-
-
+        <div>
+    <select onChange={(e)=>setSortType(e.target.value)} className='border-2 text-sm mb-5 ml-4' name="" id="">
+        <option value="relavent">Sort by:Relavent</option>
+        <option value="low-high">Sort by: Low to High</option>
+        <option value="high-low">Sort by: High to Low</option>
+      </select>
     </div>
-  )
+      </div>
+      <div className=' w-[65%] md:w-[65%] lg:w-[65%] gap-2 mx-auto grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+        {latestCollection.map((item, index) => { return (<ProductCom key={index} id={item.id} src={item.src} dis={item.dis} price={item.price} />) })}
+      </div>
+    </div>
+  </div>
+);
 }
+
 
 export default Latest
--- frontend/src/Components/ProductContext.jsx
+++ frontend/src/Components/ProductContext.jsx
@@ -8,65 +8,74 @@ const ProductContext=({children})=>{
         {
             src:"https://images.pexels.com/photos/6069085/pexels-photo-6069085.jpeg",
             dis:"jacket red color",
-            price:230,
+            price:210,
             isbest:true,
-            id:1
+            id:1,
+            cat:"men"
         },
         {
             src:"https://images.pexels.com/photos/2697896/pexels-photo-2697896.jpeg",
             dis:"jacket red color",
             price:230,
             isbest:false,
-            id:2
+            id:2,
+            cat:"women"
         },
         {
             src:"https://images.pexels.com/photos/11855627/pexels-photo-11855627.jpeg",
             dis:"jacket red color",
-            price:230,
+            price:200,
             isbest:true,
-            id:3
+            id:3,
+            cat:"kids"
         },
         {
             src:"https://images.pexels.com/photos/6069085/pexels-photo-6069085.jpeg",
             dis:"jacket red color",
-            price:230,
+            price:190,
             isbest:false,
-            id:4
+            id:4,
+            cat:"women"
         },
         {
             src:"https://images.pexels.com/photos/2697896/pexels-photo-2697896.jpeg",
             dis:"jacket red color",
-            price:230,
+            price:120,
             isbest:true,
-            id:5
+            id:5,
+            cat:"men"
         },
         {
             src:"https://images.pexels.com/photos/11855627/pexels-photo-11855627.jpeg",
             dis:"jacket red color",
-            price:230,
+            price:170,
             isbest:true,
-            id:6
+            id:6,
+            cat:"kids"
         },
         {
             src:"https://images.pexels.com/photos/6069085/pexels-photo-6069085.jpeg",
             dis:"jacket red color",
-            price:230,
+            price:160,
             isbest:false,
-            id:7
+            id:7,
+            cat:"men"
         },
         {
             src:"https://images.pexels.com/photos/6069085/pexels-photo-6069085.jpeg",
             dis:"jacket red color",
-            price:230,
+            price:190,
             isbest:false,
-            id:8
+            id:8,
+            cat:"kids"
         },
         {
             src:"https://images.pexels.com/photos/6069085/pexels-photo-6069085.jpeg",
             dis:"jacket red color",
-            price:230,
+            price:300,
             isbest:true,
-            id:9
+            id:9,
+            cat:"kids"
         },
   
     
